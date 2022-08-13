eef90e772f510d0a84ccea74521a117462b7ffc3
Harald <leithner@itronic.at>
2022-08-13 14:31:34 +0200

    Build preview for pull request

--- test.sh
+++ test.sh
@@ -13,4 +13,4 @@ fi
 curl -X POST "https://api.github.com/repos/$DRONE_REPO/statuses/$DRONE_COMMIT" \
   -H "Content-Type: application/json" \
   -H "Authorization: token $GITHUB_TOKEN" \
-  -d "{\"state\": \"success\", \"context\": \"$HEADLINE\", \"description\": \"DESCRIPTION\", \"target_url\": \"TARGETURL\"}" > /dev/null
+  -d "{\"state\": \"success\", \"context\": \"$HEADLINE\", \"description\": \"$DESCRIPTION\", \"target_url\": \"$TARGETURL\"}" > /dev/null
